200fa6713366d38a40eb2aa01a3a9f22cd869d17
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-02-26 15:01:40 +0100

    update main.ts for live publish

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+
+  // Uppdaterad kod för att använda Render-porten
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0'); 
+
+  console.log(`🚀 Server is running on port ${port}`);
 }
 bootstrap();
